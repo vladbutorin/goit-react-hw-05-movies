80197a5132f2e5b0dc07a0d28ce76222a207fd21
Vlad <89925292+vladbutorin@users.noreply.github.com>
2023-11-27 17:51:40 +0200

    movies закончил

---
+++ src/components/Form/Form.styled.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  padding: 8px 8px;
+  font: inherit;
+  cursor: pointer;
+  border-radius: 4px;
+  color: #191d1e;
+  font-weight: 600;
+  font-size: 20px;
+  margin-right: 10px;
+`;
+
+export const Button = styled.button`
+  padding: 8px 8px;
+  font: inherit;
+  cursor: pointer;
+  border-radius: 4px;
+  color: #191d1e;
+  font-weight: 600;
+  font-size: 20px;
+`;
